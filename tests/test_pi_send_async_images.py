6fdbc012cd7fbf04961566c2d15cf8b66f6e22f1
patrickryan <theyoungsoul@gmail.com>
2019-07-05 13:15:50 -0500

    added server-ip argument to the test_pi_send file

--- tests/test_pi_send_async_images.py
+++ tests/test_pi_send_async_images.py
@@ -5,18 +5,29 @@ from imagezmq.asyncimagesender import AsyncImageSender
 from imutils.video import VideoStream
 import imutils as imutils
 import socket
+import argparse
+
 
 """
 Test file that will create 2 AsyncImageSender classes and send an image to the specified server and port
 
 """
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-s", "--server-ip", required=False, default='192.168.1.208',
+                help="ip address of the server to which the client will connect")
+
+args = vars(ap.parse_args())
+server_ip = args['server_ip']
+
 # get the host name, initialize the video stream, and allow the
 # camera sensor to warmup
 rpiName = socket.gethostname()
 
 video_stream = VideoStream(usePiCamera=True).start()
 
-async_image_sender1 = AsyncImageSender(server_name=rpiName, server_ip='192.168.1.208', port=5555, send_timeout=10, recv_timeout=10, show_frame_rate=10)
+async_image_sender1 = AsyncImageSender(server_name=rpiName, server_ip=server_ip, port=5555, send_timeout=10, recv_timeout=10, show_frame_rate=10)
 async_image_sender1.run_in_background()
 
 image_count = 0
